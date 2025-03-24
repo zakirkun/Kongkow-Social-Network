73b653e1497e428475c3c24358f521a1b29b1858
Muhammad Zakir Ramadhan <61570975+zakirkun@users.noreply.github.com>
2025-03-24 12:03:20 +0700

    init base

---
+++ src/main/java/app/kongkow/social/KongkowSocialNetworkApplication.java
@@ -0,0 +1,13 @@
+package app.kongkow.social;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KongkowSocialNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KongkowSocialNetworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/kongkow/social/KongkowSocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package app.kongkow.social;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KongkowSocialNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
